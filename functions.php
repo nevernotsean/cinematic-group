9e8798adbd495dd5b6917573b37cec42398ce7e7
Sean Harrington <sean@never-not.com>
2017-07-22 15:11:41 -0400

    add images from old theme, fix releases page
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- functions.php
+++ functions.php
@@ -49,9 +49,9 @@ class StarterSite extends TimberSite {
 	}
 
 	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
+		// $context['foo'] = 'bar';
+		// $context['stuff'] = 'I am a value set in your functions.php file';
+		// $context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
 		return $context;
---
+++ timber-releases-page-custom.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Template Name: Releases Page
+ * Description: A Page Template for releases
+ */
+
+$context = Timber::get_context();
+$args = array(
+  'post_type' => 'muso-album'
+);
+$context['posts'] = Timber::get_posts($args);
+
+Timber::render( array( 'page-releases.twig' ), $context );
