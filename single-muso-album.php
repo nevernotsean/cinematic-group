b052d9eb9e0d41421f2b3b441388ab0afa42a7b4
Sean Harrington <sean@never-not.com>
2017-07-30 17:11:57 -0400

    Add artist link to album
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- single-muso-album.php
+++ single-muso-album.php
@@ -13,6 +13,27 @@ $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 
+
+$args = array(
+	'post_type' => 'muso-album',
+	'posts_per_page' => -1
+);
+$more_albums = Timber::get_posts($args);
+shuffle($more_albums);
+$context['more_albums'] = array_slice($more_albums, 1, 4);
+
+$artist_slug = wp_get_post_terms($post->ID,'artist')[0]->slug;
+$artist_args = array(
+	'post_type'   => 'portfolio',
+	'post_status' => 'publish',
+	'numberposts' => 1,
+	'name' => $artist_slug
+);
+$artist = Timber::get_post($artist_args);
+
+$context['artist_name'] = $artist->post_title;
+$context['artist_perma'] = get_permalink($artist->id);
+
 echo "<script> console.dir(" . json_encode($context) . ")</script>";
 
 if ( post_password_required( $post->ID ) ) {
