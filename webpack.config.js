b060f5e2a4b64ae068e724a858d080d535bd2f78
Sean Harrington <sean@never-not.com>
2017-08-23 20:38:42 -0400

    update the mobile menu
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
 
 module.exports = {
 	context: path.resolve(__dirname, './src'),
