576e6c520febf6153fc63494597c3e3b639af991
Sean Harrington <sean@never-not.com>
2017-09-13 11:53:59 -0400

    add overlap to home
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- timber-home-page-custom.php
+++ timber-home-page-custom.php
@@ -7,7 +7,7 @@
 $context = Timber::get_context();
 $context['page'] = Timber::get_post();
 $context['posts'] = Timber::get_posts('numberposts=5');
-// $context['news'] = Timber::get_posts('category_name=news&numberposts=3');
-// $context['features'] = Timber::get_posts('category_name=features&numberposts=2');
+$context['radio'] = Timber::get_posts('post_type=radio&numberposts=1');
+$context['features'] = Timber::get_posts('category_name=features&numberposts=1');
 // echo "<script> console.dir(" . json_encode($context) . ")</script>";
 Timber::render( array( 'page-home.twig' ), $context );
